f07c6a5513dbeff2dbebc26c4902499bcfde6fd6
unknown <nayang_etha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-08 18:39:02 +0700

    first commit

---
+++ src/latihan_2/Latihan_2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_2;
+
+/**
+ *
+ * @author MOKLET1
+ */
+public class Latihan_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Deklarasi variabel lokal
+        int nilai_pbo, nilai_web;
+        //Penyimpanan data dalam variabel
+        nilai_pbo = 90;
+        nilai_web = 80;
+        
+        System.out.println("Variabel yang terdapat dalam program :");
+        
+        //Menampilkan data dengan cara memanggil variabel
+        System.out.println("nilai_pbo = "+nilai_pbo);
+        System.out.println("nilai_web = "+nilai_web);
+    }
+    
+}
